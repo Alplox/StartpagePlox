ac13a641015292fa9e0580925091cc845f5dcec7
Alplox <42575576+Alplox@users.noreply.github.com>
2021-05-27 23:34:03 -0400

    purge

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,84 @@
+// Reloj
+function updateClock() {
+  var now = new Date();
+  var dname = now.getDay(),
+    mo = now.getMonth(),
+    dnum = now.getDate(),
+    yr = now.getFullYear(),
+    hou = now.getHours(),
+    min = now.getMinutes(),
+    sec = now.getSeconds(),
+    pe = "AM";
+
+  if (hou >= 12) {
+    pe = "PM";
+  }
+  if (hou == 0) {
+    hou = 12;
+  }
+  if (hou > 12) {
+    hou = hou - 12;
+  }
+
+  Number.prototype.pad = function (digits) {
+    for (var n = this.toString(); n.length < digits; n = 0 + n);
+    return n;
+  };
+
+  var months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  var week = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+  ];
+  var ids = [
+    "dayname",
+    "month",
+    "daynum",
+    "year",
+    "hour",
+    "minutes",
+    "seconds",
+    "period",
+  ];
+  var values = [
+    week[dname],
+    dnum.pad(2),
+    months[mo],
+    yr,
+    hou.pad(2),
+    min.pad(2),
+    sec.pad(2),
+    pe,
+  ];
+  for (var i = 0; i < ids.length; i++)
+    document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+}
+
+function initClock() {
+  updateClock();
+  window.setInterval("updateClock()", 1);
+}
+
+initClock();
+
 // https://github.com/Jaredk3nt/homepage
 // Handle writing out Bookmarks
 function setupBookmarks() {
@@ -24,7 +105,7 @@ window.onload = () => {
 
 // MODAL https://www.w3schools.com/howto/howto_css_modals.asp
 var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("Btn_skull");
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
@@ -104,3 +185,5 @@ document.onkeypress = (e) => {
     }
   };
 };
+
+console.log("\r\n$$\\      $$\\ $$$$$$$$\\ $$\\   $$\\  $$$$$$\\        $$\\      $$\\ $$$$$$$$\\ $$\\   $$\\  $$$$$$\\  \r\n$$ | $\\  $$ |$$  _____|$$$\\  $$ |$$  __$$\\       $$ | $\\  $$ |$$  _____|$$$\\  $$ |$$  __$$\\ \r\n$$ |$$$\\ $$ |$$ |      $$$$\\ $$ |$$ \/  $$ |      $$ |$$$\\ $$ |$$ |      $$$$\\ $$ |$$ \/  $$ |\r\n$$ $$ $$\\$$ |$$$$$\\    $$ $$\\$$ |$$$$$$$$ |      $$ $$ $$\\$$ |$$$$$\\    $$ $$\\$$ |$$$$$$$$ |\r\n$$$$  _$$$$ |$$  __|   $$ \\$$$$ |$$  __$$ |      $$$$  _$$$$ |$$  __|   $$ \\$$$$ |$$  __$$ |\r\n$$$  \/ \\$$$ |$$ |      $$ |\\$$$ |$$ |  $$ |      $$$  \/ \\$$$ |$$ |      $$ |\\$$$ |$$ |  $$ |\r\n$$  \/   \\$$ |$$$$$$$$\\ $$ | \\$$ |$$ |  $$ |      $$  \/   \\$$ |$$$$$$$$\\ $$ | \\$$ |$$ |  $$ |\r\n\\__\/     \\__|\\________|\\__|  \\__|\\__|  \\__|      \\__\/     \\__|\\________|\\__|  \\__|\\__|  \\__|\r\n                                                                                            \r\n                                                                                            \r\n                                                                                            \r\n")
